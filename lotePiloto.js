e5649b514c749ec5c2bafa12268722424faffc9b
Reynaldo Santos <reynaldo.santos@tksi.com.br>
2018-12-21 17:38:38 -0200

    Alterações peoplePickers

--- lotePiloto.js
+++ lotePiloto.js
@@ -1,53 +1,22 @@
-function initializePeoplePicker(peoplePickerElementId, Users) {
-    if (typeof (Users) == 'undefined') Users = null;
+// Render and initialize the client-side People Picker.
+function initializePeoplePicker(peoplePickerElementId) {
+
     // Create a schema to store picker properties, and set the properties.
     var schema = {};
     schema['PrincipalAccountType'] = 'User,DL,SecGroup,SPGroup';
     schema['SearchPrincipalSource'] = 15;
     schema['ResolvePrincipalSource'] = 15;
-    schema['AllowMultipleValues'] = false;
+    schema['AllowMultipleValues'] = true;
     schema['MaximumEntitySuggestions'] = 50;
     schema['Width'] = '280px';
-    this.SPClientPeoplePicker_InitStandaloneControlWrapper(peoplePickerElementId, Users, schema);
+
+    // Render and initialize the picker. 
+    // Pass the ID of the DOM element that contains the picker, an array of initial
+    // PickerEntity objects to set the picker value, and a schema that defines
+    // picker properties.
+    this.SPClientPeoplePicker_InitStandaloneControlWrapper(peoplePickerElementId, null, schema);
 }
 
-function registerPPOnChangeEvent(ppElement) {
-    var ppId = ppElement.attr('id') + "_TopSpan";
-    console.log(ppId);
-
-    //var addOnChanged = function(ctx) {
-    if (SPClientPeoplePicker &&
-        SPClientPeoplePicker.SPClientPeoplePickerDict &&
-        SPClientPeoplePicker.SPClientPeoplePickerDict[ppId]) {
-
-        console.log("In registerPPOnChangeEvent if");
-
-        var picker = SPClientPeoplePicker.SPClientPeoplePickerDict[ppId];
-        picker.oldChanged = picker.OnControlResolvedUserChanged;
-
-        console.log("picker");
-        console.log(picker);
-        //OnControlResolvedUserChanged
-        picker.OnControlResolvedUserChanged = function () {
-            if (picker.TotalUserCount == 0) {
-                $('#resolvedUsers').html("");
-                $('#userKeys').html("");
-                $('#userProfileProperties').html("");
-                $('#userID').html("");
-            } else {
-                setTimeout(function () {
-                    getUserInfo();
-                },
-                    100);
-            }
-            picker.oldChanged();
-        }
-    } else {
-        setTimeout(function () { addOnChanged(ctx); }, 100);
-        console.log("In registerPPOnChangeEvent else");
-    }
-    //}
-}
 // Query the picker for user information.
 function getUserInfo() {
 
@@ -56,17 +25,10 @@ function getUserInfo() {
 
     // Get information about all users.
     var users = peoplePicker.GetAllUserInfo();
-    console.log(users);
-    var owner = users[0];
-    console.log(owner);
-    $("#siteOwenerEmail").val(owner.AutoFillSubDisplayText);
-    $("#siteOwenerClaim").val(owner.Key);
-    $("#siteOwenerName").val(owner.DisplayText);
-    $("#siteOwenerLogin").val(owner.Description);
     var userInfo = '';
     for (var i = 0; i < users.length; i++) {
         var user = users[i];
-        for (var userProperty in user) {
+        for (var userProperty in user) { 
             userInfo += userProperty + ':  ' + user[userProperty] + '<br>';
         }
     }
@@ -86,14 +48,13 @@ function getUserId(loginName) {
     this.user = context.get_web().ensureUser(loginName);
     context.load(this.user);
     context.executeQueryAsync(
-        Function.createDelegate(null, ensureUserSuccess),
-        Function.createDelegate(null, onFail)
+         Function.createDelegate(null, ensureUserSuccess), 
+         Function.createDelegate(null, onFail)
     );
 }
 
 function ensureUserSuccess() {
     $('#userId').html(this.user.get_id());
-    $("#siteOwenerId").val(this.user.get_id());
 }
 
 function onFail(sender, args) {
@@ -416,6 +377,24 @@ function EscolherAgendamento() {
     }
 };
 
+function initializeAllPeoplePickers()
+{
+    initializePeoplePicker('peoplePickerRespDLPCL');
+    initializePeoplePicker('peoplePickerRespEngEnvase');
+    initializePeoplePicker('peoplePickerGerenteEngEnvase');
+    initializePeoplePicker('peoplePickerRespEngFab');
+    initializePeoplePicker('peoplePickerGerenteEngFab');
+    initializePeoplePicker('peoplePickerRespInDF');
+    initializePeoplePicker('peoplePickerGerenteInDF');
+    initializePeoplePicker('peoplePickerRespInvDE');
+    initializePeoplePicker('peoplePickerGerenteInvDE');
+    initializePeoplePicker('peoplePickerRespQualidade');
+    initializePeoplePicker('peoplePickerGerenteQualidade');
+    initializePeoplePicker('peoplePickerRespFabrica');
+    initializePeoplePicker('peoplePickerGerenteFabrica');
+    
+};
+
 $(document).ready(function () {
     CarregarLinhasDoProduto();
     CarregarCategoriaProjeto();
@@ -424,10 +403,8 @@ $(document).ready(function () {
     CarregarListaMotivos();
     CarregarFabricas();
     CarregarListaGrauComplexidade();
-    initializePeoplePicker('peoplePickerDiv');
-    registerPPOnChangeEvent($('#peoplePickerDiv'));
 
-
+    initializeAllPeoplePickers();
 
     $("#produtoControlResponsavelAmostra").hide();
     $("#produtoControlQuantidadeAmostra").hide();
