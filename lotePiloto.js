695c758b2fab3799bda09778e195b2f5594df69e
Reynaldo Santos <reynaldo.santos@tksi.com.br>
2018-12-21 16:00:46 -0200

    Teste PeoplePicker Responsáveis

--- lotePiloto.js
+++ lotePiloto.js
@@ -1,12 +1,114 @@
+function initializePeoplePicker(peoplePickerElementId, Users) {
+    if (typeof (Users) == 'undefined') Users = null;
+    // Create a schema to store picker properties, and set the properties.
+    var schema = {};
+    schema['PrincipalAccountType'] = 'User,DL,SecGroup,SPGroup';
+    schema['SearchPrincipalSource'] = 15;
+    schema['ResolvePrincipalSource'] = 15;
+    schema['AllowMultipleValues'] = false;
+    schema['MaximumEntitySuggestions'] = 50;
+    schema['Width'] = '280px';
+    this.SPClientPeoplePicker_InitStandaloneControlWrapper(peoplePickerElementId, Users, schema);
+}
+
+function registerPPOnChangeEvent(ppElement) {
+    var ppId = ppElement.attr('id') + "_TopSpan";
+    console.log(ppId);
+
+    //var addOnChanged = function(ctx) {
+    if (SPClientPeoplePicker &&
+        SPClientPeoplePicker.SPClientPeoplePickerDict &&
+        SPClientPeoplePicker.SPClientPeoplePickerDict[ppId]) {
+
+        console.log("In registerPPOnChangeEvent if");
+
+        var picker = SPClientPeoplePicker.SPClientPeoplePickerDict[ppId];
+        picker.oldChanged = picker.OnControlResolvedUserChanged;
+
+        console.log("picker");
+        console.log(picker);
+        //OnControlResolvedUserChanged
+        picker.OnControlResolvedUserChanged = function () {
+            if (picker.TotalUserCount == 0) {
+                $('#resolvedUsers').html("");
+                $('#userKeys').html("");
+                $('#userProfileProperties').html("");
+                $('#userID').html("");
+            } else {
+                setTimeout(function () {
+                    getUserInfo();
+                },
+                    100);
+            }
+            picker.oldChanged();
+        }
+    } else {
+        setTimeout(function () { addOnChanged(ctx); }, 100);
+        console.log("In registerPPOnChangeEvent else");
+    }
+    //}
+}
+// Query the picker for user information.
+function getUserInfo() {
+
+    // Get the people picker object from the page.
+    var peoplePicker = this.SPClientPeoplePicker.SPClientPeoplePickerDict.peoplePickerDiv_TopSpan;
+
+    // Get information about all users.
+    var users = peoplePicker.GetAllUserInfo();
+    console.log(users);
+    var owner = users[0];
+    console.log(owner);
+    $("#siteOwenerEmail").val(owner.AutoFillSubDisplayText);
+    $("#siteOwenerClaim").val(owner.Key);
+    $("#siteOwenerName").val(owner.DisplayText);
+    $("#siteOwenerLogin").val(owner.Description);
+    var userInfo = '';
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i];
+        for (var userProperty in user) {
+            userInfo += userProperty + ':  ' + user[userProperty] + '<br>';
+        }
+    }
+    $('#resolvedUsers').html(userInfo);
+
+    // Get user keys.
+    var keys = peoplePicker.GetAllUserKeys();
+    $('#userKeys').html(keys);
+
+    // Get the first user's ID by using the login name.
+    getUserId(users[0].Key);
+}
+
+// Get the user ID.
+function getUserId(loginName) {
+    var context = new SP.ClientContext.get_current();
+    this.user = context.get_web().ensureUser(loginName);
+    context.load(this.user);
+    context.executeQueryAsync(
+        Function.createDelegate(null, ensureUserSuccess),
+        Function.createDelegate(null, onFail)
+    );
+}
+
+function ensureUserSuccess() {
+    $('#userId').html(this.user.get_id());
+    $("#siteOwenerId").val(this.user.get_id());
+}
+
+function onFail(sender, args) {
+    alert('Query failed. Error: ' + args.get_message());
+}
+
 function InserirAgendamento() {
     var campos = [];
 
     $('#main [name]').each(function () {
         var $this = $(this);
 
-        if($this.is('[type=checkbox]') && $this.val() != undefined) {
+        if ($this.is('[type=checkbox]') && $this.val() != undefined) {
             campos.push([this.name, $this.val() == 'on']);
-        } else if($this.val() != undefined) {
+        } else if ($this.val() != undefined) {
             campos.push([this.name, $this.val()]);
         }
     });
@@ -79,10 +181,10 @@ function CarregarAgendamento(id) {
 
             var atributos = $registro.get(0).attributes;
 
-            $.each(atributos, function() {
+            $.each(atributos, function () {
                 var $elemento = $('#main [name=' + this.name.substr(4) + ' i]');
 
-                if($elemento.is('[type=checkbox]')) {
+                if ($elemento.is('[type=checkbox]')) {
                     $elemento.attr('checked', this.value == "1");
                     $elemento.change();
                 } else {
@@ -101,7 +203,7 @@ function ResetarAgendamento() {
     $('#main [name]').each(function () {
         var $this = $(this);
 
-        if($this.is('[type=checkbox]')) {
+        if ($this.is('[type=checkbox]')) {
             $this.attr('checked', false);
             $this.change();
         } else {
@@ -110,13 +212,14 @@ function ResetarAgendamento() {
     });
 }
 
-function CarregarFabricas() {
+function CarregarLinhasEquipamentos(fabrica, tipoLote) {
     var $promise = $.Deferred();
+
     $().SPServices({
         operation: 'GetListItems',
-        listName: 'Fabricas',
-        CAMLQuery: '<Query><OrderBy><FieldRef Name="ID" /></OrderBy></Query>',
-        CAMLViewFields: '<ViewFields><FieldRef Name="ID" /><FieldRef Name="Title" /></ViewFields>',
+        listName: 'Linhas e Equipamentos',
+        CAMLQuery: '<Query><Where><And><And><Eq><FieldRef Name="Ativa" /><Value Type="Boolean">1</Value></Eq><Eq><FieldRef Name="Fabrica" /><Value Type="Lookup">' + fabrica + '</Value></Eq></And><Eq><FieldRef Name="TipoLote" /><Value Type="Choice">' + tipoLote + '</Value></Eq></And></Where></Query>',
+        CAMLViewFields: '<ViewFields><FieldRef Name="Title" /><FieldRef Name="ID" /></ViewFields>',
         completefunc: function (Data, Status) {
             if (Status != 'success') {
                 $promise.reject({
@@ -127,26 +230,58 @@ function CarregarFabricas() {
                 return;
             }
 
-            // var atributos = $(Data.responseText).find('z\\:row:first').get(0).attributes;
-
-            // $.each(atributos, function () {
-            //     var $elemento = $('#main [name=' + this.name.substr(4) + ' i]');
-
-            //     if ($elemento.is('[type=checkbox]')) {
-            //         $elemento.attr('checked', this.value == "1");
-            //         $elemento.change();
-            //     } else {
-            //         $elemento.val(this.value);
-            //     }
-            // });
+            $(Data.responseXML).SPFilterNode("z:row").each(function () {
+                $('select#linhaEquipamento').append('<option value="' + $(this).attr("ows_ID") + '">' + $(this).attr("ows_Title") + '</option>')
+            })
 
             $promise.resolve();
         }
     });
 
     return $promise;
-
 }
+
+function CarregarFabricas() {
+    var $promise = $.Deferred();
+
+    $().SPServices({
+        operation: 'GetListItems',
+        listName: 'Fábricas Internas e Armazenamento de Fábricas Terceiras',
+        CAMLViewFields: '<ViewFields><FieldRef Name="Title" /><FieldRef Name="ID" /><FieldRef Name="Numero" /></ViewFields>',
+        completefunc: function (Data, Status) {
+            if (Status != 'success') {
+                $promise.reject({
+                    errorCode: '0x99999999',
+                    errorText: 'Erro Remoto'
+                });
+
+                return;
+            }
+
+            $(Data.responseXML).SPFilterNode("z:row").each(function () {
+                $('select#fabrica').append('<option value="' + $(this).attr("ows_ID") + '">' + $(this).attr("ows_Title") + ' - ' + $(this).attr("ows_Numero") + '</option>')
+            })
+
+            $promise.resolve();
+        }
+    });
+
+    return $promise;
+}
+
+function CarregarListaGrauComplexidade() {
+    $().SPServices({
+        operation: 'GetList',
+        listName: 'Agendamentos',
+        completefunc: function (Data, Status) {
+            if (Status == 'success') {
+                $(Data.responseXML).find('Field[DisplayName="Grau de complexidade"] CHOICE').each(function () {
+                    $('select#grauComplexidade').append('<option value="' + this.innerHTML + '">' + this.innerHTML + '</option>');
+                });
+            }
+        }
+    });
+};
 
 function CarregarListaStatus() {
     $().SPServices({
@@ -168,7 +303,7 @@ function CarregarListaMotivos() {
         listName: 'Agendamentos',
         completefunc: function (Data, Status) {
             if (Status == 'success') {
-                $(Data.responseXML).find('Field[DisplayName="Motivo "] CHOICE').each(function () {
+                $(Data.responseXML).find('Field[DisplayName="Motivo"] CHOICE').each(function () {
                     $('select#motivo').append('<option value="' + this.innerHTML + '">' + this.innerHTML + '</option>');
                 });
             }
@@ -221,7 +356,7 @@ function CarregarLinhasDoProduto() {
 function EscolherAgendamento() {
     var agendamentoId = prompt('Digite o ID do agendamento');
 
-    if(agendamentoId) {
+    if (agendamentoId) {
         ResetarAgendamento();
 
         CarregarAgendamento(agendamentoId).fail(function (response) {
@@ -236,6 +371,12 @@ $(document).ready(function () {
     CarregarListaStatus();
     CarregarListaTiposLotes();
     CarregarListaMotivos();
+    CarregarFabricas();
+    CarregarListaGrauComplexidade();
+    initializePeoplePicker('peoplePickerDiv');
+    registerPPOnChangeEvent($('#peoplePickerDiv'));
+
+
 
 
     $("#tabs").tabs();
