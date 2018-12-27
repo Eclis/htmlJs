56cfc1973401c40feaab4e6e1b895e097d2dc110
Reynaldo Santos <reynaldo.santos@tksi.com.br>
2018-12-27 12:58:25 -0200

    Teste carregamento Agendamentos

---
+++ src/agendamentos.js
@@ -0,0 +1,32 @@
+function CarregarTodosAgendamentos() {
+    var $promise = $.Deferred();
+
+    $().SPServices({
+        operation: 'GetListItems',
+        listName: 'Agendamentos',
+        CAMLViewFields: '<ViewFields><FieldRef Name="ID" /><FieldRef Name="CodigoProduto" /><FieldRef Name="Title" /><FieldRef Name="TipoLote" /><FieldRef Name="Motivo" /><FieldRef Name="Status" /><FieldRef Name="RegistroAnalisesInicio" /><FieldRef Name="Modified" /><FieldRef Name="Editor" /><FieldRef Name="InicioProgramado" /><FieldRef Name="_UIVersionString" /></ViewFields>',
+        completefunc: function (Data, Status) {
+            if (Status != 'success') {
+                $promise.reject({
+                    errorCode: '0x99999999',
+                    errorText: 'Erro Remoto'
+                });
+
+                return;
+            }
+
+            $(Data.responseXML).SPFilterNode("z:row").each(function () {
+                $('tbody#AgendamentoBody').append('<tr><td>'+$(this).attr("ows_ID")+'</td><td>'+$(this).attr("ows_CodigoProduto")+'</td><td>'+$(this).attr("ows_Title")+'</td><td>'+$(this).attr("ows_TipoLote")+'</td><td>'+$(this).attr("ows_Motivo")+'</td><td>'+$(this).attr("ows_Status")+'</td><td>'+$(this).attr("ows_RegistroAnalisesInicio")+'</td><td>'+$(this).attr("ows_Modified")+'</td><td>'+$(this).attr("ows_Editor")+'</td><td>'+$(this).attr("ows_InicioProgramado")+'</td><td>'+$(this).attr("ows__UIVersionString")+'</td></tr>')
+            });
+
+            $promise.resolve();
+        }
+    });
+
+    return $promise;
+}
+
+
+$(document).ready(function () {
+    CarregarTodosAgendamentos();
+});
