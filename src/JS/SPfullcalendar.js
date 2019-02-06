7c908cdb932168d9f2408dd62453e1cb4b9c6dc5
José Nicodemos Maia Neto <jose+github.com@nicomaia.com.br>
2019-02-06 19:54:03 -0200

    Alterações do Farkas no Calendário

--- src/JS/SPfullcalendar.js
+++ src/JS/SPfullcalendar.js
@@ -1,5 +1,5 @@
 
-var PATH_TO_DISPFORM = "https://naturabr.sharepoint.com/sites/DEV_LotePiloto/Lists/Agendamentos/DispForm.aspx";
+var PATH_TO_DISPFORM = _spPageContextInfo.webAbsoluteUrl  + "/Lists/Agendamentos/DispForm.aspx";
 var TASK_LIST = "Agendamentos";
 var Fabrica_LIST = "Fábricas Internas e Armazenamento de Fábricas Terceiras";
 var coresFabricas = new Array();
@@ -94,10 +94,10 @@ function DisplayTasks() {
             var RESTQuery = "";
 
             if (FabricaSelecionada != "*") {
-                RESTQuery = "/_api/Web/Lists/GetByTitle('" + TASK_LIST + "')/items?$select=ID,Title,Status,InicioProgramado,FimProgramado,CodigoProduto,DescricaoProduto,TipoLote,Fabrica/ID,Fabrica/Chave&$expand=Fabrica&$filter=Fabrica/ID eq " + FabricaSelecionada;
+                RESTQuery = "/_api/Web/Lists/GetByTitle('" + TASK_LIST + "')/items?$select=ID,Title,Status,HaveraAcompanhamento,InicioProgramado,FimProgramado,CodigoProduto,DescricaoProduto,TipoLote,Fabrica/ID,Fabrica/Chave&$expand=Fabrica&$filter=Fabrica/ID eq " + FabricaSelecionada;
             }
             else {
-                RESTQuery = "/_api/Web/Lists/GetByTitle('" + TASK_LIST + "')/items?$select=ID,Title,Status,InicioProgramado,FimProgramado,CodigoProduto,DescricaoProduto,TipoLote,Fabrica/ID,Fabrica/Chave&$expand=Fabrica";
+                RESTQuery = "/_api/Web/Lists/GetByTitle('" + TASK_LIST + "')/items?$select=ID,Title,Status,HaveraAcompanhamento,InicioProgramado,FimProgramado,CodigoProduto,DescricaoProduto,TipoLote,Fabrica/ID,Fabrica/Chave&$expand=Fabrica";
             }
 
             var opencall = $.ajax({
@@ -110,28 +110,41 @@ function DisplayTasks() {
             });
 
             opencall.done(function (data, textStatus, jqXHR) {
+				var cores = ["#446AC1", "#E490AE", "#FFA565", "#B2B208", "#88D73C", "#2A9983", "#EDA700", "#8B2F9E", "#999090", "#E40000"];
+
                 var events = [];
                 for (index in data.d.results) {
                     var fabrica = "";
                     var titulo = data.d.results[index].CodigoProduto;
-                    var descricaoProduto = data.d.results[index].DescricaoProduto;
+					var descricaoProduto = data.d.results[index].DescricaoProduto;
                     var tipoLote = data.d.results[index].TipoLote;
                     var cor = "#E40000";
                     var inicio = moment.utc(data.d.results[index].InicioProgramado).local();
                     var fim = moment.utc(data.d.results[index].FimProgramado).local();
-                    if (data.d.results[index].Fabrica.Chave) {
+					var haveraAcompanhamento = (data.d.results[index].HaveraAcompanhamento === "1")
+					var strHaveraAcompanhamento = haveraAcompanhamento ? 'Sim' : 'Não';
+                    // try{
+					if (data.d.results[index].Fabrica.Chave) {
                         fabrica = data.d.results[index].Fabrica.Chave;
                         cor = (coresFabricas.filter(obj => { return obj.id === data.d.results[index].Fabrica.ID }))[0].cor
                     }
-
+					// }
+					
+					// catch(err){
+						// alert(err);
+					// }
+					if(haveraAcompanhamento){
+						titulo = '\u2690' + " " +titulo;
+					}
                     events.push({
                         // title: data.d.results[index].Title ,
                         title: titulo,
                         id: data.d.results[index].ID,
+						border : haveraAcompanhamento ? "dashed" : "solid",
                         color: cor, //specify the background color and border color can also create a class and use className paramter. 
                         start: inicio.format('YYYY-MM-DD HH:mm'),
                         end: fim.format('YYYY-MM-DD HH:mm'), //add one day to end date so that calendar properly shows event ending on that day
-                        description: descricaoProduto + "<br /><b>Tipo de Lote: </b>" + tipoLote + "<br /><b>Fábrica: </b>" + fabrica + "<br /><b>Início: </b>" + inicio.format('DD-MM-YYYY HH:mm') + "<br /><b>Fim: </b>" + fim.format('DD-MM-YYYY HH:mm')
+                        description: descricaoProduto + "<br /><b>Tipo de Lote: </b>" + tipoLote + "<br /><b>Fábrica: </b>" + fabrica + "<br /><b> Haverá acompanhamento: </b>" + strHaveraAcompanhamento + "<br /><b>Início: </b>" + inicio.format('DD-MM-YYYY HH:mm') + "<br /><b>Fim: </b>" + fim.format('DD-MM-YYYY HH:mm')
                     });
                 }
 
